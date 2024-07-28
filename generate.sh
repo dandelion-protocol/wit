23fea4f0e20b121c58802bccc3c38abda1f9edae
Donald King <chronos@chronos-tachyon.net>
2024-07-28 15:13:14 -0700

    Rename dandelion:core to dandelion:common.

--- generate.sh
+++ generate.sh
@@ -3,11 +3,12 @@ set -euo pipefail
 cd "$(dirname "$0")"
 readonly CWD="$(pwd)"
 
-readonly PACKAGES=( core daemon )
+readonly PACKAGES=( common daemon )
+readonly VERSION="0.1.0"
 
 rm -rf out
 mkdir -p out
 for package in "${PACKAGES[@]}"; do
   cd "${CWD}/${package}"
-  wit build -o "${CWD}/out/${package}.wasm"
+  wit build -o "${CWD}/out/dandelion-${package}@${VERSION}.wasm"
 done
